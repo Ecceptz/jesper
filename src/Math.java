84b41c26655c0d18ea097b1b0a4d1d2d4cabd9d7
Ecceptz <Jesperebklad97@hotmail.com>
2019-12-03 09:57:25 +0100

    Added Sub method to the Math class

--- src/Math.java
+++ src/Math.java
@@ -5,4 +5,11 @@ public class Math {
         int third = first + second;
         return third;
     }
+
+
+    public int sub(int first, int second)   {
+
+        int third = first - second;
+        return third;
+    }
 }
